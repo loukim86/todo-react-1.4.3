ca1576d231146ecb5cf4957872c26d735d974e63
Luiza Kim <kimluisa2886@gmail.com>
2024-01-20 22:37:57 +0900

    my first commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import Header from "../Header/Header";
 import TaskList from "../TaskList/TaskList";
 import Footer from "../Footer/Footer";
@@ -6,20 +6,52 @@ import Footer from "../Footer/Footer";
 import './App.css';
 
 const App = () => {
-    const taskData = [
+    const [taskData, setTaskData] = useState([
         { id: 1, status: 'completed', description: 'Completed task', created: 'created 17 seconds ago' },
         { id: 2, status: 'editing', description: 'Editing task', created: 'created 5 minutes ago' },
         { id: 3, status: '', description: 'Active task', created: 'created 5 minutes ago' }
-    ]
-    return (
-        <section className="todoapp">
-            <Header />
-            <section className="main">
-                <TaskList taskData={taskData} />
-                <Footer />
-            </section>
-        </section>
-    );
-}
+    ]);
 
-export default App
+    const editTask = (id) => {
+      setTaskData((prevTaskData) => {
+          const updatedTaskData = prevTaskData.map((item) => {
+              if (item.id === id) {
+                  const taskStatus = item.status === 'completed' ? '' : 'completed';
+                  return { ...item, status: taskStatus };
+              }
+              return item;
+          });
+          return updatedTaskData;
+      });
+  };
+
+  const deleteTask = (id) => {
+    setTaskData((prevTaskData) => {
+        const updatedTaskData = prevTaskData.filter((item) => item.id !== id);
+        return updatedTaskData;
+    });
+};
+
+return (
+  <section className="todoapp">
+      <Header />
+      <section className="main">
+          <TaskList taskData={taskData}
+           onChange={editTask}
+           onDelete={deleteTask} />
+          <Footer />
+      </section>
+  </section>
+ );
+};
+
+export default App;
+
+
+
+
+    
+
+   
+
+    
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,30 +2,31 @@ import React from 'react';
 import Task from "../Task/Task";
 import './TaskList.css';
 
-
-const TaskList = ({ taskData }) => {
-    const taskTodo = taskData.map((item) => {
-
-      const {id, status, ...itemProps} = item;
-        return (
-            <li key={item.id} className={item.status}>
-                <div className="view">
-                    <input className="toggle" type="checkbox"/>
-                    <label><Task taskItem={itemProps} /></label>
-                    <button className="icon icon-edit"></button>
-                    <button className="icon icon-destroy"></button>
-                </div>
-                {item.status === 'editing' ? <input type="text" className="edit" value="Editing task"/> : null}
-            </li>
-        );
-    });
-
+const TaskList = ({ taskData, onChange, onDelete }) => {
+  const taskTodo = taskData.map((item) => {
+    const {id, status, ...itemProps} = item;
     return (
-        <ul className="todo-list">
-            { taskTodo }
-        </ul>
+        <li key={id} className={status}>
+            <div className="view">
+                <input className="toggle" type="checkbox"
+                       checked={status === 'completed' ? true : false}
+                       onChange={() => onChange(id)}/>
+                <label><Task taskItem={itemProps}/></label>
+                <button className="icon icon-edit"></button>
+                <button className="icon icon-destroy"
+                 onClick={() => onDelete(id)}></button>
+            </div>
+            {status === 'editing' ? <input type="text" className="edit" defaultValue="Editing task"/> : null}
+        </li>
     );
+});
+
+return (
+    <ul className="todo-list">
+        {taskTodo}
+    </ul>
+);
 }
 
-
 export default TaskList;
+
